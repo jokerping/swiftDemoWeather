9384105fea490ae9ea7c1e36f0da5be4b0237d22
王志平 <wangzhiping1@100tal.com>
2018-11-21 20:59:07 +0800

    first commit

---
+++ weather/Tool/MainNetManger.swift
@@ -0,0 +1,13 @@
+//
+//  MainNetManger.swift
+//  weather
+//
+//  Created by 王志平 on 2018/11/21.
+//  Copyright © 2018 王志平. All rights reserved.
+//
+
+import UIKit
+
+class MainNetManger: NSObject {
+
+}
---
+++ weather/model/WeatherModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherModel.swift
+//  weather
+//
+//  Created by 王志平 on 2018/11/21.
+//  Copyright © 2018 王志平. All rights reserved.
+//
+
+import UIKit
+
+class WeatherModel: NSObject {
+
+}
